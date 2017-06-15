ab114ebe6119e73bc8a19594962ef5711de11562
Michael Panson <mpanson@kenzan.com>
2017-06-15 11:21:34 -0600

    logging changes

--- src/main/java/ribboneureka/server/controller/SearchBackendController.java
+++ src/main/java/ribboneureka/server/controller/SearchBackendController.java
@@ -1,5 +1,7 @@
 package ribboneureka.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,13 @@ import ribboneureka.server.dto.SearchResponse;
 @RequestMapping(path = "search")
 public class SearchBackendController {
 
+    private static Logger logger = LoggerFactory.getLogger(SearchBackendController.class);
+
     @RequestMapping(method = RequestMethod.GET, path = "/{query}")
     public ResponseEntity<SearchResponse> getSearch(
             @PathVariable(name = "query", required = true) String query) {
-        return new ResponseEntity(new SearchResponse(String.join("-", query, "result")), HttpStatus.OK);
+        SearchResponse searchResponse = new SearchResponse(String.join("-", query, "result"));
+        logger.info("Returning search response: " + searchResponse);
+        return new ResponseEntity(searchResponse, HttpStatus.OK);
     }
 }
